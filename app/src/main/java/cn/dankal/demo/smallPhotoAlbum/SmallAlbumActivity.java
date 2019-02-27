0c660828dac74febaf630b6ec2b8257dfeed8475
BatesLi <libates52@gamil.com>
2019-02-27 16:59:41 +0800

    提供图片与title数据

--- app/src/main/java/cn/dankal/demo/smallPhotoAlbum/SmallAlbumActivity.java
+++ app/src/main/java/cn/dankal/demo/smallPhotoAlbum/SmallAlbumActivity.java
@@ -4,13 +4,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.dankal.basic_lib.util.ToastUtil;
 import cn.dankal.demo.R;
 
 public class SmallAlbumActivity extends AppCompatActivity {
 
   @BindView(R.id.toolbar_album) Toolbar mToolbarAlbum;
+  @BindView(R.id.img_photo) ImageView mImgPhoto;
+  @BindView(R.id.txt_photo_information) TextView mTxtPhotoInformation;
+  @BindView(R.id.btn_previous_photo) Button mBtnPreviousPhoto;
+  @BindView(R.id.btn_next_photo) Button mBtnNextPhoto;
+
+  int[] imageViews = {R.drawable.leaf_autumn, R.drawable.leaf_drop, R.drawable.leaf_red
+      , R.drawable.leaf_wind};
+
+  String[] photoInformation = {"第一张", "第二张", "第三张", "第四张"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,7 @@ public class SmallAlbumActivity extends AppCompatActivity {
     setContentView(R.layout.activity_small_album);
     ButterKnife.bind(this);
     setToolbar();
+    setPhotoBtn();
   }
 
   public void setToolbar() {
@@ -28,6 +42,17 @@ public class SmallAlbumActivity extends AppCompatActivity {
     mToolbarAlbum.setNavigationOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         finish();
+      }
+    });
+  }
+
+  public void setPhotoBtn() {
+    mBtnNextPhoto.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        for (int i = 0; i < photoInformation.length; i++) {
+          mTxtPhotoInformation.setText(photoInformation[i]);
+        }
+        ToastUtil.toToast("下一张");
       }
     });
   }
